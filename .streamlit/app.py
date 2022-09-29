0303daaa49a33578f38fe16f898d55e7d3c929d7
adutiny <113724666+adutiny@users.noreply.github.com>
2022-09-29 07:59:25 -0700

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -24,8 +24,8 @@ contact_form = """
        <input type="text" name="downpayment" placeholder="Have Down payment Y/N"required>
         <input type="text" name="budget" placeholder="Budget?"required>
          <input type="text" name="Loan" placeholder="Need Loan?(Y/N)"required>
-          <input type="text" name="FICO" placeholder="FICO above 650?(Y/N)"required>
-          <input type="text" name="location" placeholder=" Location for Tiny Y/N"required>
+          <input type="text" name="FICO" placeholder="FICO above 680?(Y/N)"required>
+          <input type="text" name="location" placeholder=" Location for Tiny Home"required>
            <input type="text" name="Wheels" placeholder="Tiny House on Wheels(Y/N)"required> 
            <input type="text" name="foundation" placeholder="Tiny on a foundation(Y/N)"required>
            <input type="hidden" name="_captcha" value="false">
